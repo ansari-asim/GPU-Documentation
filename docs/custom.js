84504f0ad2525341abf4b9d436c8c91fcd1babf4
ansari-asim <asimansari400097@gmail.com>
2025-02-21 12:25:22 +0530

    mkdocs change

---
+++ docs/custom.js
@@ -0,0 +1,13 @@
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6798ec6d825083258e0c3d1c/1iimln353';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
